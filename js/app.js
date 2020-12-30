de20dd8ee91a7b76c7d1b2061653ae5b1eb997bc
UTfrank <frankjoe261@gmail.com>
2020-12-30 16:32:38 +0100

    changed the email registration section of the reg form

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 // javascript file for the api calls
 
+const axios = require('axios').default;
+
+// function for grabbing ids
+function _(str) {
+    return document.querySelector(str);
+}
+
 // Set base url using axios global defaults
 axios.defaults.baseURL = "https://dcibackend.herokuapp.com/";
+
